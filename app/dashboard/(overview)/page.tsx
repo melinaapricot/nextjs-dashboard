a0393e22586bcc0c12ad10036ae4156261d759e9
Melina Verykiou <melinaverikios@tuta.io>
2024-04-25 14:12:26 +0300

    add titles

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,7 +10,11 @@ import {
   CardsSkeleton,
 } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 export default async function Page() {
   return (
     <main>
